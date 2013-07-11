90ff6b649d083147d23746c794cef305b931fb71
johndavid400 <jd@isotope11.com>
2013-07-11 15:31:41 -0500

    added config initializer option for api key

--- lib/edmunds/api.rb
+++ lib/edmunds/api.rb
@@ -10,10 +10,20 @@ module Edmunds
     attr_reader :base, :api_key, :image_base_url, :format, :base_url
 
     def initialize
-      @base = "http://api.edmunds.com/v1/api/vehicle"
-      @api_key = ENV["EDMUNDS_VEHICLE"]
-      @image_base_url = "http://media.ed.edmunds-media.com"
-      @format = "fmt=json&api_key=#{@api_key}"
+      if set_key
+        @base = "http://api.edmunds.com/v1/api/vehicle"
+        @image_base_url = "http://media.ed.edmunds-media.com"
+        @format = "fmt=json&api_key=#{@api_key}"
+      else
+        return @errors
+      end
+    end
+
+    def set_key
+      @api_key = ENV["EDMUNDS_VEHICLE"] || Rails.configuration.edmunds_vehicle_api_key
+    rescue
+      @errors = "You need to set your Edmunds Vehicle API key first"
+      return false
     end
 
     def call_api
